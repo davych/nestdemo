691f16b3c3386f737e0403df670f105b837d01b1
davy_chen <davy_chen@epam.com>
2022-11-17 21:48:45 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
+import { RolesGuard } from './guards/roles.guard';
+
+@Module({
+  imports: [CatModule],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    AppService,
+  ],
+})
+export class AppModule {}

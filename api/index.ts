13c7521c6ba81820c2af28a4d9c600fcef612a4c
davy_chen <davy_chen@epam.com>
2022-11-17 22:32:36 +0800

    done

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { AppModule } from '../src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
